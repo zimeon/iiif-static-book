2cbc5f4413b283cc6d2775dcc128998bff6de4a6
Simeon Warner <simeon.warner@cornell.edu>
2017-08-29 08:12:55 -0400

    Writes a (bad) manifest

--- iiif-static-book.py
+++ iiif-static-book.py
@@ -10,6 +10,18 @@ import sys
 from iiif import IIIFStatic
 from iiif_prezi.factory import ManifestFactory
 
+
+class Page(object):
+
+    def __init__(self, image_file=None, identifier=None):
+        self.image_file = image_file
+        self.identifier = identifier
+
+    @property
+    def info_json(self):
+        return(self.identifier + '/info.json')
+
+
 def main():
     """Parse arguments, do it..."""
     if (sys.version_info < (3, 4)):
@@ -26,6 +38,8 @@ def main():
     p.add_argument('--base-prezi-dir', default='')
     p.add_argument('--manifest_uri', default='http://localhost:9876/book/manifest.json')
     # Control of static tiles
+    p.add_argument('--skip-tiles', action='store_true',
+                   help="Skip tile generation (assume has already been done on previous run")
     p.add_argument('--tilesize', action='store', type=int, default=512,
                  help="Tilesize in pixels")
     p.add_argument('--api-version', action='store', default='2.1',
@@ -54,26 +68,30 @@ def main():
                     prefix=args.base_image_uri, osd_version='2.0.0')
     for src in args.src:
         logging.warning("Looking at %s..." % (src))
-        for page in sorted(glob.glob(os.path.join(src, args.pattern))):
-            logging.warn("Page %s..." % (page))
-            sg.generate(page)
-            pages.append(page)
+        for image_file in sorted(glob.glob(os.path.join(src, args.pattern))):
+            identifier = os.path.split(os.path.splitext(image_file)[0])[1]
+            logging.warn("Page %s..." % (image_file))
+            if (not args.skip_tiles):
+                sg.generate(image_file)   
+            pages.append(Page(image_file=image_file,
+                              identifier=identifier))
     # Write manifest
     fac = ManifestFactory()
     fac.set_debug("error")
-    fac.set_base_image_uri(args.base_image_uri)
+    fac.set_base_image_uri("file:" + image_dir)  # use local image file info.json
     fac.set_base_image_dir(image_dir)
     fac.set_iiif_image_info()
     fac.set_base_prezi_uri(args.base_prezi_uri)
     fac.set_base_prezi_dir(prezi_dir)
-    mflbl = os.path.split(image_dir)[1].replace("_", " ").title()
+    mflbl = "Book " + os.path.split(image_dir)[1].replace("_", " ")
     mfst = fac.manifest(label=mflbl)
     seq = mfst.sequence()
     for page in pages:
-        ident = os.path.splitext(page)[0]
-        title = ident.replace("_", " ").title()
-        cvs = seq.canvas(ident=ident, label=title)
-        cvs.add_image_annotation(ident, True)
+        ident = page.identifier
+        title = ident.replace("_", " ")
+        print("Title=" + title)
+        canvas = seq.canvas(ident=ident, label=title)
+        canvas.set_image_annotation(ident, True)
     mfst.toFile(compact=False)
 
 if __name__ == '__main__':
